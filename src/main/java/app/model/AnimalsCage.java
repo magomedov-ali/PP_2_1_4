0b065d77b3282713a2715b7a287a87a43f617ccd
magomedov-ali <magomedov.ali.2000@gmail.com>
2023-10-15 19:02:49 +0300

    constructor-based DI

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -7,12 +7,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class AnimalsCage {
 
-    @Autowired
-    @Qualifier("dog")
-    private Animal animal;
+    private final Animal animal;
+
+    private final Timer timer;
 
     @Autowired
-    private Timer timer;
+    public AnimalsCage(@Qualifier("dog") Animal animal, Timer timer) {
+        this.animal = animal;
+        this.timer = timer;
+    }
 
     public Timer getTimer() {
         return timer;
